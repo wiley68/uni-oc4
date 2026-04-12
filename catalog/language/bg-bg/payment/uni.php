fb7173e79c42bae3a5a8c52e9bb7980300088833
Ilko Ivanov <ilko.iv@gmail.com>
2026-04-12 12:25:28 +0000

    payment method translation

--- catalog/language/bg-bg/payment/uni.php
+++ catalog/language/bg-bg/payment/uni.php
@@ -1,8 +1,6 @@
 <?php
 
-$_['heading_title'] = 'На кредит с УниКредит Кънсюмър Файненсинг';
-
+$_['heading_title'] = 'УниКредит покупки на Кредит';
 $_['text_installment_months_comment'] = 'Избрани месеци за изплащане (UniCredit): %s';
-
 $_['error_order']          = 'Поръчката не е намерена.';
 $_['error_payment_method'] = 'Невалиден метод на плащане.';
--- catalog/language/en-gb/payment/uni.php
+++ catalog/language/en-gb/payment/uni.php
@@ -1,8 +1,6 @@
 <?php
 
-$_['heading_title'] = 'UniCredit Consumer Financing';
-
+$_['heading_title'] = 'UniCredit Credit purchases';
 $_['text_installment_months_comment'] = 'Selected installment months (UniCredit): %s';
-
 $_['error_order']          = 'Order could not be found.';
 $_['error_payment_method'] = 'Invalid payment method.';
